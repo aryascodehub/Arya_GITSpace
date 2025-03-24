ef558cec780a385266e351d039e970bac8e457c6
aryascodehub <66656976+aryascodehub@users.noreply.github.com>
2025-03-24 18:44:03 -0500

    Combine Strings.py

--- Combine Strings.py
+++ Combine Strings.py
@@ -4,7 +4,7 @@
 intro = str(input('''Please enter a string that is at least more than 2 characters:'''))
 
 if len(intro) <= 2:
-    print('STOP IT SILLY BOY! - Be sure to enter more than 2 characters next time...')
+    print('STOP IT! - Be sure to enter more than 2 characters next time...')
 else:
     print("...and the answer is...." + intro[0:2] + intro[-2:len(intro)])  # negative indexing and slicing is used in
     # this
